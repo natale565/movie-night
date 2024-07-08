52a4560f0d31277c434c0b2d8f97623f586473f9
Chris Natale <natale565@icloud.com>
2024-07-08 19:09:09 -0400

    removed modal and added form to HTML

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const searchFormEl = document.getElementById('taskForm');
+
+searchFormEl.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const movieTitle = document.getElementById('movieTitle').value;
+    const genre = document.getElementById('genre').value;
+    const year = document.getElementById('year').value;
+    
+    if (movieTitle !== "") {
+        console.log("Movie title is filled out. Form can be submitted.");
+    
+        if ((genre !== "" && year === "") || (year !== "" && genre === "")) {
+            console.log("Either genre or year is filled out. The other is required.");
+            // Add your form submission logic here
+            const post = { movieTitle, genre, year };
+            let posts = JSON.parse(localStorage.getItem('movies')) || [];
+            posts.push(post);
+            localStorage.setItem('movies', JSON.stringify(posts));
+    
+            // Redirect to search results page
+            window.location.href = 'search-results.html';
+        } else {
+            console.log("Either genre or year should be filled out, and the other is required.");
+        }
+    } else {
+        console.log("Movie title is required.");
+    }
+});
