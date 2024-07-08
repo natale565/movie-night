9b283e3380625824b47233e38491eef0792056c0
Scott Reynolds <scottreynolds6294@gmail.com>
2024-07-08 19:14:59 -0400

    OMDB Api logic started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-
 const openModalBtn = document.getElementById('openModal');
 const modal = document.getElementById('modal');
 const closeBtn = document.querySelector('.close');
+const omdbApiKey = 'af9c33c6';
+
 
 openModalBtn.addEventListener('click', function() {
     modal.style.display = 'block';
@@ -16,3 +17,30 @@ window.addEventListener('click', function(event) {
         modal.style.display = 'none';
     }
 });
+
+
+function fetchOmdbData(data){
+const movieTitle = document.getElementbyId('movieInput').value.trim();
+ if (movieTitle === '') {
+    alert('Please enter movie title');
+    return;
+ }
+ const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(movieTitle)}`;
+
+ fetch(apiUrl)
+ .then(response => response.json())
+ .then(data => {
+    if (data.response === 'True') {
+        const movieDetails = `
+        <h2> ${data.Title} ($data.Year) </h2>
+        <p>Plot:${data.plot}</p>
+        <p>IMDB Rating:${data.imdbRating}</p>`
+    console.log(movieDetails)
+    } else {
+        console.log('Movie not found');
+    }
+ })
+ .catch(error => {
+    console.error('Error fetching data', error);
+ });
+}
